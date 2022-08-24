1da9a9854f9329f9e0c9d734e7d22267bcaa93fb
Jackson Sin <35521578+jacksonsin@users.noreply.github.com>
2022-08-24 13:22:18 +0800

    Delete module.py

--- module.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    def __init__(self, first, last, pay):
-        self.first = first.capitalize()
-        self.last = last.capitalize()
-        self.pay = pay
-        self.email = first.lower() + "." + last.lower() + "@gmail.com"
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
